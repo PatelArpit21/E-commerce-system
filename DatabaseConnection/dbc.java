cd0505f169ebe5960ef7e5ac22ef0edd135d0b23
PatelArpit21 <patelarpit4242@gmail.com>
2025-09-19 22:20:36 +0530

    Add files via upload

---
+++ DatabaseConnection/dbc.java
@@ -0,0 +1,14 @@
+package DatabaseConnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+//DONE
+public class dbc {
+    private static final String URL = "jdbc:mysql://localhost:3306/ecommerce";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
